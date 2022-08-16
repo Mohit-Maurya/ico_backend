2dabbbae0c3e8f76a182cea8b3517e802d0c5388
AMAN KUMAR <workaman1138@gmail.com>
2022-08-16 20:29:00 +0530

    Data Models

--- models/investors.model.js
+++ models/investors.model.js
@@ -4,38 +4,38 @@ const Schema = mongoose.Schema;
 
 export const InvestorSchema = new Schema({
     name: {
-        type:String,
-        // required:true
+        type: String,
+        required: true
     },
     pan: {
-        type:String,
-        // required:true
+        type: String,
+        required: true
     },
     aadhaar: {
-        type:String,
-        // required:true
+        type: String,
+        required: true
     },
     phone_number: {
-        type:String,
-        // required:true
+        type: String,
+        required: true
     },
     email: {
-        type:String,
-        // required:true
+        type: String,
+        required: true
     },
-    bank_id:{
-        type:mongoose.Schema.Types.ObjectId,
-        // required:true,
-        ref:'BankAccount'
+    bank_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'BankAccount'
     },
-    
+
     status: {
-        type:String
+        type: String
     },
     password: {
-        type:String
+        type: String
     }
 
-}, 
-{ collection : 'investors' }
+},
+    { collection: 'investors' }
 )
