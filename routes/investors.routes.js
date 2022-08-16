7f6c8650ca1f0aece3a0a6e0eb18d6469e28160c
sweety879 <seelasweety38@gmail.com>
2022-08-16 20:40:41 +0530

    investor

--- routes/investors.routes.js
+++ routes/investors.routes.js
@@ -1,7 +1,7 @@
-import { addNewUser } from "../controllers/investors.controller";
+import { addNewInvestor } from "../controllers/investors.controller";
 
 const routes = (app) => {
-    app.route("/investors").post(addNewUser);
+    app.route("/investors").post(addNewInvestor);
 }
 
 export default routes;
