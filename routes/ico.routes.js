8e314ff5ad57e3936eb45d80f9ab344a5533d8c2
Mohit Maurya <mohitmaurya126@gmail.com>
2022-08-18 13:11:05 +0530

    new api

--- routes/ico.routes.js
+++ routes/ico.routes.js
@@ -1,7 +1,7 @@
 import { addNewBidding, bidsPerStatus ,getBidbyCoin,bidUpdate,bidsByInvestor} from "../controllers/biddings.controllers";
 import { addNewInvestor, LoginInvestor } from "../controllers/investors.controller";
 import { addNewDeveloper, LoginDeveloper } from "../controllers/developers.controller";
-import { allCoins, addNewCoin,coinById } from "../controllers/coins.controller";
+import { allCoins, addNewCoin,coinById, getCoinWithDevId } from "../controllers/coins.controller";
 import { allocate } from "../controllers/coins.controller";
 
 const routes = (app) => {
@@ -9,30 +9,30 @@ const routes = (app) => {
     app.route("/investors/login").post(LoginInvestor);
     app.route("/developers/login").post(LoginDeveloper)
     app.route("/developers").post(addNewDeveloper);
+    app.route("/")
 
     //coins api
     app.route("/coins")
         .get(allCoins)
         .post(addNewCoin);
 
-    app.route("/coin/:id")
-        .get(coinById);   
+    app.route("/coin/:id").get(coinById);  
+    app.route("/coins/:developerId").get(getCoinWithDevId);
     //app.route("/date-of-coins").get(dateOfCoins);
     //bidding api
-    app.route("/biddings")
-        .post(addNewBidding);
-    app.route("/bids-per-status/:investorId/:status").get(bidsPerStatus)
+    app.route("/biddings").post(addNewBidding);
+    app.route("/bids-per-status/:investorId/:status").get(bidsPerStatus);
     //allocation engine api
-    app.route("/coin/allocation/:coinId")
-        .get(allocate);
+    app.route("/coin/allocation/:coinId").get(allocate);
 
     //bidding api
-    app.route("/biddings")
-        .post(addNewBidding);
-    app.route("/get-bid-by-coin/:coinId/:investorId").get(getBidbyCoin)
-    app.route("/editBid").post(bidUpdate)
+    app.route("/biddings").post(addNewBidding);
+    app.route("/get-bid-by-coin/:coinId/:investorId").get(getBidbyCoin);
+    app.route("/editBid").post(bidUpdate);
+    
 
-    app.route("/get-bid-by-investor/:id").get(bidsByInvestor)
+    app.route("/get-bid-by-investor/:id").get(bidsByInvestor);
+
 
     //timer api
 }
